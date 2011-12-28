d07b9acb6aaca0ab94e3db7a94cdaf2efbcf7a55
eiu165 <eiu165@gmail.com>
2011-12-28 16:03:35 -0600

    adding code templates

---
+++ Source/Web/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidPub.Web.Model;
+using VidPub.Web.Infrastructure;
+namespace Web.Controllers{
+    public class UsersController : CruddyController {
+        public UsersController(ITokenHandler tokenStore):base(tokenStore) {
+            _table = new Users();
+            ViewBag.Table = _table;
+        }
+    }
+}
+
